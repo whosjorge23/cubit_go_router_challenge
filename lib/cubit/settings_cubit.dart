14564cd5674bb170f5c84e7750f3eff530c76066
Giorgio Giannotta <jorge_upa@msn.com>
2023-09-11 07:32:11 +0200

    Refactor save and load function to have a try catch

--- lib/cubit/settings_cubit.dart
+++ lib/cubit/settings_cubit.dart
@@ -22,18 +22,28 @@ class SettingsCubit extends Cubit<SettingsState> {
   }
 
   // Save the current state to SharedPreferences
-  void saveStateToSharedPreferences() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setDouble('volumeValue', state.volumeValue);
-    await prefs.setDouble('luminosityValue', state.luminosityValue);
+  Future<void> saveStateToSharedPreferences() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setDouble('volumeValue', state.volumeValue);
+      await prefs.setDouble('luminosityValue', state.luminosityValue);
+    } catch (e) {
+      // Handle any potential errors while saving to SharedPreferences
+      print("Error saving state to SharedPreferences: $e");
+    }
   }
 
   // Load the state from SharedPreferences and emit a new state
   Future<void> loadStateFromSharedPreferences() async {
-    final prefs = await SharedPreferences.getInstance();
-    final volumeValue = prefs.getDouble('volumeValue') ?? 0;
-    final luminosityValue = prefs.getDouble('luminosityValue') ?? 0;
-    emit(SettingsState.initial(volumeValue, luminosityValue));
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final volumeValue = prefs.getDouble('volumeValue') ?? 0;
+      final luminosityValue = prefs.getDouble('luminosityValue') ?? 0;
+      emit(SettingsState.initial(volumeValue, luminosityValue));
+    } catch (e) {
+      // Handle any potential errors while loading from SharedPreferences
+      print("Error loading state from SharedPreferences: $e");
+    }
   }
-
 }
+
